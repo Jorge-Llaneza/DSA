1157a2d11d9d3bb07da9589cd95552e4768c2546
jorge <llanezarodriguezjorge@gmail.com>
2026-06-04 18:34:07 +0200

    coded nested recursion

---
+++ nested-recursion.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+int fun(int n);
+
+int main()
+{
+    fun(95);
+    return 0;
+}
+
+int fun(int n)
+{
+    printf("%d ", n);
+
+    if (n> 100)
+    {
+        return n - 10;
+    }
+    else 
+    {
+        return fun(fun(n +11));
+    }
+}
