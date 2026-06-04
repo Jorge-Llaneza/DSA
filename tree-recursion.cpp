74b1c3f86483d77c855d3ce57d0349f43b472212
jorge <llanezarodriguezjorge@gmail.com>
2026-06-04 18:19:19 +0200

    initial commin

---
+++ tree-recursion.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <cstdlib>
+
+void fun(int n)
+{
+    if (n>0)
+    {
+        printf("%d ", n);
+        fun(n-1);
+        fun(n-1);
+    }
+}
+
+int main() {
+    fun(4);
+    return EXIT_SUCCESS;
+}
