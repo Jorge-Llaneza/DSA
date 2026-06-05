2b623660484b5fe00feac2c8cbcdc391bd58d76c
jorge <llanezarodriguezjorge@gmail.com>
2026-06-05 13:57:21 +0200

    coded factorial

---
+++ factorial.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+
+int factorial(int n);
+int iterative_factorial(int n);
+
+int main() 
+{
+    printf("%d \n", factorial(12));
+    printf("%d \n", iterative_factorial(12));
+}
+
+// O(n) time and space
+int factorial(int n)
+{
+    if (n <= 0) 
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial(n-1);
+    }
+}
+
+// O(n) time but O(1) space
+int iterative_factorial(int n) 
+{
+    int prod = 1;
+    for (int i = 1; i <= n; i++) 
+    {
+        prod *= i;
+    }
+    return prod;
+}
